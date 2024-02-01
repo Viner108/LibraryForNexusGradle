4beb81d676f67767714d74c0faaa84aa06d1f037
Viner108 <andrukovichl@mail.ru>
2024-02-01 14:04:57 +0300

    Initial commit

---
+++ src/main/java/com/lid/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Unit {
+    private int row;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+}
